73215035bbf26124483cce897dc4c6e963a4b4da
Kenneth Shaw <kenshaw@gmail.com>
2021-04-05 07:58:18 +0700

    Minor change to imdb.go

--- imdb.go
+++ imdb.go
@@ -46,32 +46,32 @@ type Rating struct {
 
 // A MovieResult will hold the related information of a single movie.
 type MovieResult struct {
-	Title             string
-	Year              string
-	Rated             string
-	Released          string
-	Runtime           string
-	Genre             string
-	Director          string
-	Writer            string
-	Actors            string
-	Plot              string
-	Language          string
-	Country           string
-	Awards            string
-	Poster            string
-	Metascore         string
-	ImdbRating        string
-	ImdbVotes         string
-	ImdbID            string
-	Type              string
-	Ratings           []Rating
-	DVD               string
-	BoxOffice         string
-	Production        string
-	Website           string
-	Response          string
-	Error             string
+	Title      string
+	Year       string
+	Rated      string
+	Released   string
+	Runtime    string
+	Genre      string
+	Director   string
+	Writer     string
+	Actors     string
+	Plot       string
+	Language   string
+	Country    string
+	Awards     string
+	Poster     string
+	Metascore  string
+	ImdbRating string
+	ImdbVotes  string
+	ImdbID     string
+	Type       string
+	Ratings    []Rating
+	DVD        string
+	BoxOffice  string
+	Production string
+	Website    string
+	Response   string
+	Error      string
 }
 
 // String satisifies the Stringer interface for MovieResult.
