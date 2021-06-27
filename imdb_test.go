49f03299167ef2b7e87acf6bdd2a40aa1ae3d526
Kenneth Shaw <kenshaw@gmail.com>
2021-06-27 08:55:58 +0700

    Adding unit test for FindGame

--- imdb_test.go
+++ imdb_test.go
@@ -4,32 +4,9 @@ import (
 	"testing"
 )
 
-func TestFindName(t *testing.T) {
-	t.Parallel()
-	res, err := FindName("brad pitt", WithLogf(t.Logf), WithAppCacheDir("imdb-test"))
-	if err != nil {
-		t.Fatalf("expected no error, got: %v", err)
-	}
-	if len(res) < 1 {
-		t.Fatalf("expected at least one search result, got: %d", len(res))
-	}
-	if exp := "Brad Pitt"; res[0].Title != exp {
-		t.Errorf("expected %q, got: %q", exp, res[0].Title)
-	}
-	if exp, got := "", res[0].Year(); got != exp {
-		t.Errorf("expected %q, got: %q", exp, got)
-	}
-	if exp, got := "nm", res[0].Subtype; got != exp {
-		t.Errorf("expected %q, got: %q", exp, got)
-	}
-	if exp, got := "nm0000093", res[0].ID; got != exp {
-		t.Errorf("expected %q, got: %q", exp, got)
-	}
-}
-
 func TestFindTitle(t *testing.T) {
 	t.Parallel()
-	res, err := FindTitle("bobs burgers", WithLogf(t.Logf), WithAppCacheDir("imdb-test"))
+	res, err := FindTitle("bob burger", WithLogf(t.Logf), WithAppCacheDir("imdb-test"))
 	if err != nil {
 		t.Fatalf("expected no error, got: %v", err)
 	}
@@ -50,8 +27,53 @@ func TestFindTitle(t *testing.T) {
 	}
 }
 
+func TestFindName(t *testing.T) {
+	t.Parallel()
+	res, err := FindName("brd pitt", WithLogf(t.Logf), WithAppCacheDir("imdb-test"))
+	if err != nil {
+		t.Fatalf("expected no error, got: %v", err)
+	}
+	if len(res) < 1 {
+		t.Fatalf("expected at least one search result, got: %d", len(res))
+	}
+	if exp := "Brad Pitt"; res[0].Title != exp {
+		t.Errorf("expected %q, got: %q", exp, res[0].Title)
+	}
+	if exp, got := "", res[0].Year(); got != exp {
+		t.Errorf("expected %q, got: %q", exp, got)
+	}
+	if exp, got := "nm", res[0].Subtype; got != exp {
+		t.Errorf("expected %q, got: %q", exp, got)
+	}
+	if exp, got := "nm0000093", res[0].ID; got != exp {
+		t.Errorf("expected %q, got: %q", exp, got)
+	}
+}
+
+func TestFindGame(t *testing.T) {
+	res, err := FindGame("wrld warcraft", WithLogf(t.Logf), WithAppCacheDir("imdb-test"))
+	if err != nil {
+		t.Fatalf("expected no error, got: %v", err)
+	}
+	if len(res) < 1 {
+		t.Fatalf("expected at least one search result, got: %d", len(res))
+	}
+	if exp := "World of Warcraft"; res[0].Title != exp {
+		t.Errorf("expected %q, got: %q", exp, res[0].Title)
+	}
+	if exp, got := "2004", res[0].Year(); got != exp {
+		t.Errorf("expected %q, got: %q", exp, got)
+	}
+	if exp, got := "vg", res[0].Subtype; got != exp {
+		t.Errorf("expected %q, got: %q", exp, got)
+	}
+	if exp, got := "tt0433664", res[0].ID; got != exp {
+		t.Errorf("expected %q, got: %q", exp, got)
+	}
+}
+
 func TestFindMovie(t *testing.T) {
-	res, err := FindMovie("fight club", WithLogf(t.Logf), WithAppCacheDir("imdb-test"))
+	res, err := FindMovie("fight club 1999", WithLogf(t.Logf), WithAppCacheDir("imdb-test"))
 	if err != nil {
 		t.Fatalf("expected no error, got: %v", err)
 	}
